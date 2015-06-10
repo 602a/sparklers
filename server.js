3e0a33da16aafdca2ca4a8c159d1cd22cd85c674
Eddie Kong <eddiek93@gmail.com>
2015-06-09 23:55:42 -0700

    basic mvp setup

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var http = require('http');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+var http = require('http');
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'html');
+app.set('views', './views');
+app.use(express.static(__dirname + '/views'));
+require('./routes')(app);
+
+var server = require('http').createServer(app);
+
+var port = process.env.PORT || 8000;
+  
+//Start the server
+server.listen(port, function() {
+console.log('Server listening on port ' + port);
+});
+
+module.exports = app;
